95f09d18290ecd3a96d990e7dc7bb8ed495319b2
Dao Minh Khoi <khoidm2812@gmail.com>
2020-09-01 00:08:10 +0700

    su kien scroll cho cac btn-scroll

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
 let buttonMenu = document.getElementById("btn-icon");
 let menu = document.getElementById("menu");
 let buttonScroll = document.getElementById("btn-scroll");
+let buttonScroll2 = document.getElementById("btn-scroll-2");
 
 // them su kien
 buttonMenu.addEventListener("click", barButton);
 buttonScroll.addEventListener("click", scrollButton);
+buttonScroll2.addEventListener("click", scrollButton2);
 // function
 
 function barButton(e) {
@@ -22,6 +24,15 @@ function scrollButton(e) {
   e.preventDefault();
   window.scrollTo({
     top: 500,
+    left: 0,
+    behavior: "smooth",
+  });
+}
+
+function scrollButton2(e) {
+  e.preventDefault();
+  window.scrollTo({
+    top: 1000,
     left: 0,
     behavior: "smooth",
   });
