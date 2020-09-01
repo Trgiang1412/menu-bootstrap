9c0b4b4ed19bc7562ee2b7ffbc9d41b57b563d65
Dao Minh Khoi <khoidm2812@gmail.com>
2020-09-01 15:27:51 +0700

    switch branch

--- main.js
+++ main.js
@@ -1,39 +1,42 @@
 // khai bao bien
+
 let buttonMenu = document.getElementById("btn-icon");
 let menu = document.getElementById("menu");
 let buttonScroll = document.getElementById("btn-scroll");
 let buttonScroll2 = document.getElementById("btn-scroll-2");
 
 // them su kien
+
 buttonMenu.addEventListener("click", barButton);
 buttonScroll.addEventListener("click", scrollButton);
 buttonScroll2.addEventListener("click", scrollButton2);
+
 // function
 
 function barButton(e) {
-  e.preventDefault();
-  menu.classList.toggle("active");
-  if (buttonMenu) {
-    buttonMenu.classList.toggle("fa-times");
-  } else {  
-    buttonMenu.classList.remove("fa-bars");
-  }
+    e.preventDefault();
+    menu.classList.toggle("active");
+    if (buttonMenu) {
+        buttonMenu.classList.toggle("fa-times");
+    } else {
+        buttonMenu.classList.remove("fa-bars");
+    }
 }
 
 function scrollButton(e) {
-  e.preventDefault();
-  window.scrollTo({
-    top: 500,
-    left: 0,
-    behavior: "smooth",
-  });
+    e.preventDefault();
+    window.scrollTo({
+        top: 500,
+        left: 0,
+        behavior: "smooth",
+    });
 }
 
 function scrollButton2(e) {
-  e.preventDefault();
-  window.scrollTo({
-    top: 1000,
-    left: 0,
-    behavior: "smooth",
-  });
+    e.preventDefault();
+    window.scrollTo({
+        top: 1000,
+        left: 0,
+        behavior: "smooth",
+    });
 }
